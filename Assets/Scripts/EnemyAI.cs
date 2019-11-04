2e44c8ede5e1b330c2797bb105b02d58f01bbea1
SineadHoward <43648438+SineadHoward@users.noreply.github.com>
2019-11-04 15:25:28 +0000

    first commit

---
+++ Assets/Scripts/EnemyAI.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class EnemyAI : MonoBehaviour
+
+{ 
+    private Transform goal;
+    private NavMeshAgent agent;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        goal = Camera.main.transform;
+        agent = GetComponent<NavMeshAgent>();
+        //set the navmesh agent's desination equal to the main camera's position (our first person character)
+        agent.destination = goal.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    void OnTriggerEnter(Collider col)
+    {
+       // GetComponent<CapsuleCollider>().enabled = false;
+        //stop the zombie from moving forward by setting its destination to it's current position
+        agent.destination = gameObject.transform.position;
+    }
+
+    }
---
+++ Assets/Scripts/Player/Raycasting.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Raycasting : MonoBehaviour
+{
+
+    public static string voiceRec = "";
+
+
+
+    // Start is called before the first frame update
+    void Start() 
+    {
+
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        //check to ee if player is shooting
+        CheckForShooting();
+    }
+    //checks to see if player is shooting
+    private  void CheckForShooting()
+    {
+        if (voiceRec == "fire")
+        {
+            RaycastHit whatIHit;
+            if (Physics.Raycast(transform.position, transform.forward, out whatIHit, Mathf.Infinity))
+            { 
+              
+                IDamageable damageable = whatIHit.collider.GetComponent<IDamageable>();
+                if (damageable !=null ) {
+                    damageable.DealDamage(10);
+                    voiceRec = "";
+                }
+            }
+        }
+    }
+}
